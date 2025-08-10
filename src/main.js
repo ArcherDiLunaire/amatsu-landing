83f6c900b7b9f61880bcf86756f961c90291723d
ArcherDilunaire <aurelrichard00@gmail.com>
2025-08-10 20:44:40 +0200

    new dist

--- src/main.js
+++ src/main.js
@@ -192,8 +192,8 @@
 
 		window.addEventListener('devicemotion', function (event) {
 			if (isTouchDevice) {
-				parallaxPos.x = curve(smoothX(-event.accelerationIncludingGravity.x / 100)) * -20;
-				parallaxPos.y = curve(smoothY(-event.accelerationIncludingGravity.y / 100)) * 5 + 1;
+				parallaxPos.x = curve(smoothX(-event.accelerationIncludingGravity.x / 100)) * -25;
+				parallaxPos.y = curve(smoothY(-event.accelerationIncludingGravity.y / 100)) * 20 + 1;
 			}
 		});
 
